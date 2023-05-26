88516dd5445873278bd092f0ee354c51149240af
Jan Bölsche <regular.gonzales@googlemail.com>
2023-05-26 10:40:37 +0200

    Can now flash Launchpad 110, using 100kHz clock speed and openocd-git

--- src/board_config.h
+++ src/board_config.h
@@ -4,7 +4,12 @@
 
 #define CPU_CLOCK_SPEED 48000000
 
-#define DIO_LED_R 0
-#define DIO_LED_G 2
+// Flytta PCB
+//#define DIO_LED_R 0
+//#define DIO_LED_G 2
+
+// Launchpad
+#define DIO_LED_R 6
+#define DIO_LED_G 7
 
 #endif /* _BOARD_CONFIG_H_INCLUDED_ */
